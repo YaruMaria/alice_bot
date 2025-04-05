d233e34b711c3f971b27694f8b9de67a810f67a8
Yarusova Maria <yarusova.maria@yandex.ru>
2025-04-05 13:56:49 +0300

    alice

--- hooks.py
+++ hooks.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')  # Укажите путь к вашему репозиторию
+        repo = git.Repo('https://github.com/YaruMaria/alice_bot.git')  # Укажите путь к вашему репозиторию
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
